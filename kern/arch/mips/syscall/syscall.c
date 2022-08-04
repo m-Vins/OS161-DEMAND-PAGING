35dba1ae1e4086bf98e9e27a92f9047dcce770a2
VINCENZO MEZZELA S294999 <s294999@studenti.polito.it>
2022-08-04 17:16:38 +0200

    added basic support for write-read-exit syscalls

--- kern/arch/mips/syscall/syscall.c
+++ kern/arch/mips/syscall/syscall.c
@@ -32,7 +32,6 @@
 #include <kern/syscall.h>
 #include <lib.h>
 #include <mips/trapframe.h>
-#include <thread.h>
 #include <current.h>
 #include <syscall.h>
 
@@ -80,7 +79,7 @@ syscall(struct trapframe *tf)
 {
 	int callno;
 	int32_t retval;
-	int err;
+	int err = 0;
 
 	KASSERT(curthread != NULL);
 	KASSERT(curthread->t_curspl == 0);
@@ -110,6 +109,28 @@ syscall(struct trapframe *tf)
 		break;
 
 	    /* Add stuff here */
+#if OPT_SYSCALLS
+	    case SYS_write:
+	        retval = sys_write((int)tf->tf_a0,
+				(userptr_t)tf->tf_a1,
+				(size_t)tf->tf_a2);
+		/* error: function not implemented */
+                if (retval<0) err = ENOSYS; 
+		else err = 0;
+                break;
+	    case SYS_read:
+	        retval = sys_read((int)tf->tf_a0,
+				(userptr_t)tf->tf_a1,
+				(size_t)tf->tf_a2);
+		/* error: function not implemented */
+                if (retval<0) err = ENOSYS; 
+		else err = 0;
+                break;
+	    case SYS__exit:
+	        /* TODO: just avoid crash */
+ 	        sys__exit((int)tf->tf_a0);
+                break;
+#endif
 
 	    default:
 		kprintf("Unknown syscall %d\n", callno);
