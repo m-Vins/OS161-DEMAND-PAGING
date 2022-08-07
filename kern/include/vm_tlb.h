4c65a30130871418732578c4fba45fc638b4a2bd
riccardotornesello <riccardo.tornesello@gmail.com>
2022-08-07 19:43:42 +0200

    add tlb_remove comment

--- kern/include/vm_tlb.h
+++ kern/include/vm_tlb.h
@@ -8,6 +8,8 @@
  *
  *  tlb_insert: associate a vaddress to a paddress in the tlb.
  *      Set ro to true to insert as read-only.
+ * 
+ *  tlb_remove: remove a virtual address from the TLB if is present.
  */
 void tlb_invalidate(void);
 void tlb_insert(vaddr_t vaddr, paddr_t paddr, bool ro);
