d93d2760bc0206a296dad864a311dca2f7ba841f
VINCENZO MEZZELA S294999 <s294999@studenti.polito.it>
2022-08-10 10:43:15 +0200

    fixed missing libary in segment.c + some update

--- kern/vm/segment.c
+++ kern/vm/segment.c
@@ -1,18 +1,23 @@
-
-
-
 #include <segment.h>
+#include <lib.h>
 
 
 struct segment *segment_create(void){
     struct segment *seg = kmalloc(sizeof(struct segment));
+
+    KASSERT(seg != NULL);
+
     seg->elf_offset = 0;
     seg->base_vaddr = 0;
     seg->npages = 0;
+    
+    return seg;
 }
 
 
 void segment_define(struct segment *seg, off_t elf_offset, vaddr_t base_vaddr, size_t npages){
+
+    KASSERT(seg != NULL);
 
     seg->elf_offset = elf_offset;
     seg->base_vaddr = base_vaddr;
