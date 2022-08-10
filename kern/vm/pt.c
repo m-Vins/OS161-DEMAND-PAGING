2c9d14b2a7849b5d02c2aa2f09d7d94e953d995e
VINCENZO MEZZELA S294999 <s294999@studenti.polito.it>
2022-08-10 13:04:14 +0200

    added pt_get_index

--- kern/vm/pt.c
+++ kern/vm/pt.c
@@ -5,6 +5,55 @@
 #include <current.h>
 #include <kern/errno.h>
 
+/**
+ * @brief Compute the page table index of the virtual address.
+ * 
+ * Check the segment from which the vaddr belongs to. 
+ * In case it belongs either to data or text segment, the page table 
+ * index is computed by means of a division.
+ * 
+ * Whereas, if the vaddr belongs to the stack segment, it must be taken 
+ * into account the logic behind the page table as it does not have an 
+ * entry for each page of the virtual address space. Indeed, the free 
+ * region between the stack and the data segments is totally skipped
+ * in the array of the page table entries. Thus, the number of skipped
+ * pages must be substracted to compute the index in the page table.
+ * 
+ *  _________________
+ * | stack           |
+ * |_________________|
+ * |                 |
+ * |                 |
+ * | free            |
+ * |_________________|
+ * | data            |
+ * |_________________|
+ * | text            |
+ * |_________________|
+ * 
+ * 
+ * @param as address space
+ * @param vaddr virtual address
+ * @return int page table index
+ */
+static int pt_get_index(struct addrspace *as,vaddr_t vaddr){
+    int pt_index;
+
+    if (vaddr >= as->s_text->base_vaddr && vaddr < as->s_data->base_vaddr + as->s_data->npages * PAGE_SIZE)
+    {
+        pt_index = vaddr / PAGE_SIZE;
+        return pt_index;
+    }
+
+    if (vaddr >= as->s_stack->base_vaddr && vaddr < as->s_stack->base_vaddr + as->s_stack->npages)
+    {
+        pt_index = as->s_text->npages + as->s_data->npages +  (vaddr - as->s_stack->base_vaddr) / PAGE_SIZE ;
+        return pt_index;
+    }
+
+    panic("vaddr out of range?!\n");
+    return 0;
+}
 
 struct pt_entry *pt_create(unsigned long pagetable_size)
 {
@@ -30,22 +79,10 @@ struct pt_entry *pt_create(unsigned long pagetable_size)
 
 struct pt_entry *pt_get_entry(const vaddr_t vaddr)
 {
-    int pt_index;
-    struct addrspace *cur_as;
-
-    cur_as = curproc->p_addrspace;
-
-    if (vaddr >= cur_as->s_text->base_vaddr && vaddr < cur_as->s_data->base_vaddr + cur_as->s_data->npages * PAGE_SIZE)
-    {
-        pt_index = vaddr / PAGE_SIZE;
-        return &curproc->p_ptable[pt_index];
-    }
-
-    if (vaddr >= cur_as->s_stack->base_vaddr && vaddr < cur_as->s_stack->base_vaddr + cur_as->s_stack->npages)
-    {
-        pt_index = cur_as->s_text->npages + cur_as->s_data->npages + (vaddr - cur_as->s_stack->base_vaddr)/PAGE_SIZE ;
-        return &curproc->p_ptable[pt_index];
-    }
-
-    return NULL;
+    
+    struct addrspace *cur_as = proc_getas();
+    
+    int pt_index = pt_get_index(cur_as, vaddr);
+    
+    return &curproc->p_ptable[pt_index];
 }
