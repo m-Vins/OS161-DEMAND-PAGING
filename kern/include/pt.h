e0798eb1aba2c5c253f0f4c316baf63f3aa3bc0e
VINCENZO MEZZELA S294999 <s294999@studenti.polito.it>
2022-08-10 11:56:14 +0200

    added macros in page table

--- kern/include/pt.h
+++ kern/include/pt.h
@@ -3,10 +3,19 @@
 
 #include <types.h>
 
+#define NOT_LOADED 0
+#define IN_MEMORY 1
+#define IN_SWAP 2
+
+#define IS_IN_MEMORY(pt_entry) (pt_entry->status==IN_MEMORY)
+#define IS_IN_SWAP(pt_entry) (pt_entry->status==IN_SWAP)
+#define IS_NOT_LOADED(pt_entry) (pt_entry->status==NOT_LOADED)
+
 struct pt_entry
 {
     unsigned int frame_index : 20;
     unsigned int swap_index : 12;
+    unsigned char status : 2;
 };
 
 struct pt_entry *pt_get_entry(const vaddr_t vaddr);
