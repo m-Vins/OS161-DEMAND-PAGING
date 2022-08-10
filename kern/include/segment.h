13b33b5b8933ee6d0123831a5563c4796d87791e
VINCENZO MEZZELA S294999 <s294999@studenti.polito.it>
2022-08-10 12:18:37 +0200

    added segment_destroy

--- kern/include/segment.h
+++ kern/include/segment.h
@@ -11,8 +11,8 @@ struct segment {
 };
 
 struct segment *segment_create(void);
-void segment_define(struct segment *seg, off_t elf_offset, vaddr_t base_vaddr, size_t npages); //prende tutti i campi e il puntatore al segmento
-
+void segment_define(struct segment *seg, off_t elf_offset, vaddr_t base_vaddr, size_t npages); 
+void segment_destroy(struct segment *seg);
 
 
 #endif 
--- kern/vm/segment.c
+++ kern/vm/segment.c
@@ -18,9 +18,20 @@ struct segment *segment_create(void){
 void segment_define(struct segment *seg, off_t elf_offset, vaddr_t base_vaddr, size_t npages){
 
     KASSERT(seg != NULL);
+    KASSERT(seg->elf_offset == 0);
+    KASSERT(seg->base_vaddr == 0);
+    KASSERT(seg->npages == 0);
 
     seg->elf_offset = elf_offset;
     seg->base_vaddr = base_vaddr;
     seg->npages = npages;
 
+}
+
+
+void segment_destroy(struct segment *seg){
+    
+    KASSERT(seg != NULL);
+
+    kfree(seg);
 }
