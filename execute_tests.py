100d3efc436588149eaef94ceeaa981ce171bbf8
VINCENZO MEZZELA S294999 <s294999@studenti.polito.it>
2022-09-08 20:06:01 +0200

    tests script update

--- execute_tests.py
+++ execute_tests.py
@@ -1,5 +1,6 @@
 import pexpect
 import sys
+import os
 
 
 def getprompt(proc, prompt):
@@ -10,7 +11,9 @@ def getprompt(proc, prompt):
 			"sys161: Elapsed ",	# sys161 shutdown print
 			pexpect.EOF,
 			pexpect.TIMEOUT
-		])
+		    ],
+            timeout=3600
+        ) #large timeout due to ctest program which takes more time
 	if which == 0:
 		# got the prompt
 		return None
@@ -34,7 +37,7 @@ def getprompt(proc, prompt):
 def open_instance():
     proc = pexpect.spawn("bash", ignore_sighup=False, encoding='utf-8')
     #proc.logfile_read = sys.stdout
-    proc.sendline("cd /home/os161user/os161/root;sys161 kernel")
+    proc.sendline(f'cd {os.getenv("HOME",default=None)}/os161/root;sys161 kernel')
 
     msg = getprompt(proc, "OS/161 kernel [? for menu]: ")
     if msg:
@@ -56,16 +59,23 @@ def close_instance(proc):
     proc.close()
     return [r.split(" ")[-1] for r in test_result_rows]
 
+def kill_instance(proc):
+    proc.close()
+
 
 def run_program(proc, test):
-    print("Running program " + test)
+    return run_cmd(proc,"p testbin/" + test)
 
-    proc.sendline("p testbin/" + test)
+
+def run_cmd(proc,cmd):
+    print("Running command \"" + cmd + "\"")
+
+    proc.sendline(cmd)
     msg = getprompt(proc, "OS/161 kernel [? for menu]: ")
-    if msg:
-        return False
-    else:
+    if msg == None:
         return True
+    else:
+        return False
 
 
 def main():
@@ -85,16 +95,28 @@ def main():
     programs = [
         "palin",
         "huge",
-        "sort"
+        "sort",
+        "matmult",
+        "hugematmult1",
+        # "hugematmult2",
+        "ctest"
     ]
 
-    tests = []
+    tests = [
+        "at",
+        "at2",
+        "bt",
+        "km1",
+        "km2",
+        "km3 1000",
+        # "km4"
+    ]
 
     f = open("testresults.md", "w")
 
     # Program tests header
     f.write("# Programs\n")
-    f.write("Program name | " + " | ".join(stats) + "\n")
+    f.write("| Program name | " + " | ".join(stats) + "|\n")
     f.write("|".join(["-" for _ in range(len(stats) + 1)]) + "\n")
 
     # Program tests
@@ -104,27 +126,40 @@ def main():
         results = close_instance(proc)
 
         if success:
-            f.write(program+"|" + "|".join(results) + "\n")
+            f.write("|" + program+"|" + "|".join(results) + "|\n")
         else:
-            f.write(program+"|" + "|".join(["-" for s in stats]) + "\n")
-    f.write("\n")
-"""
-    # Generic tests header
-    f.write("# Tests\n")
-    f.write("Test name|Passed\n")
-    f.write("-|-\n")
-
-    proc = open_instance()
-    for test in tests:
-        pass # TODO
-    close_instance(proc)
+            f.write("|" + program+"|" + "|".join(["-" for s in stats]) + "|\n")
     f.write("\n")
 
+
+    # # Generic tests header
+    # f.write("# Tests\n")
+    # f.write("|Test name|Passed|\n")
+    # f.write("|-|-|\n")
+
+    # proc = open_instance()
+    # for test in tests:
+    #     success = run_cmd(proc, test)
+    #     if success:
+    #         f.write("|" + test+"|" + "True" + "|\n")
+    #     else:
+    #         print("d")
+    #         f.write("|" + test+"|" + "False" + "|\n")
+    #         kill_instance(proc)
+    #         print("c")
+    #         proc = open_instance()
+    #         print("b")
+    #     print("a")
+    # print("1")
+    # close_instance(proc)
+    # f.write("\n")
+    
+    
     # Stability test header
     f.write("# Stability test\n")
-    f.write(" | ".join(stats) + "\n")
-    f.write("|".join(["-" for s in stats]) + "\n")
-"""
+    f.write("|" + " | ".join(stats) + "|\n")
+    f.write("|" + "|".join(["-" for s in stats]) + "|\n")
+
     # Stability test
     proc = open_instance()
     failures = 0
@@ -134,7 +169,7 @@ def main():
             if not success:
                 failures += 1
     results = close_instance(proc)
-    f.write("|".join(results) + "\n")
+    f.write("|" + "|".join(results) + "|\n")
 
     print("\n")
     print("Failures: " + str(failures))
